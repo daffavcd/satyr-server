daf30c19be75d9d94fb515a9d29a7326be2b96b4
Muhammad Daffa' A.R <daffavcd@gmail.com>
2024-03-23 17:19:16 +0700

    CRUD travels endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,11 +4,12 @@ import {
   Param,
   Post,
   Body,
-  // Put,
+  Put,
   Delete,
 } from '@nestjs/common';
 import { UserService } from './user/user.service';
 import { TravelService } from './travel/travel.service';
+import { AppService } from './app.service';
 import {
   User as UserModel,
   TravelDestination as TravelDestinationModel,
@@ -17,53 +18,93 @@ import {
 @Controller()
 export class AppController {
   constructor(
+    private readonly appService: AppService,
     private readonly userService: UserService,
     private readonly travelService: TravelService,
-  ) {}
+    // eslint-disable-next-line prettier/prettier
+  ) { }
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
 
   @Get('travels/:id')
-  async getPostById(@Param('id') id: string): Promise<TravelDestinationModel> {
+  async getTravel(@Param('id') id: string): Promise<TravelDestinationModel> {
     return this.travelService.travelDestination({ id: Number(id) });
   }
 
   @Get('travels')
-  async getPublishedPosts(): Promise<TravelDestinationModel[]> {
+  async getTravels(): Promise<TravelDestinationModel[]> {
     return this.travelService.travelDestinations({
       orderBy: { id: 'desc' },
     });
   }
 
-  // @Get('filtered-posts/:searchString')
-  // async getFilteredPosts(
-  //   @Param('searchString') searchString: string,
-  // ): Promise<TravelDestinationModel[]> {
-  //   return this.travelService.travelDestinations({
-  //     where: {
-  //       OR: [
-  //         {
-  //           title: { contains: searchString },
-  //         },
-  //         {
-  //           content: { contains: searchString },
-  //         },
-  //       ],
-  //     },
-  //   });
-  // }
+  @Post('travels')
+  async createTravel(
+    @Body()
+    postData: {
+      name: string;
+      location?: string;
+      estimated_budget?: string;
+      description?: string;
+      image: string;
+      authorId: number;
+    },
+  ): Promise<TravelDestinationModel> {
+    console.log('Received POST Data:', postData);
+    const { name, location, estimated_budget, description, image, authorId } =
+      postData;
+    try {
+      return this.travelService.createTravelDestination({
+        name,
+        location,
+        estimated_budget,
+        description,
+        image,
+        author: {
+          connect: { id: authorId },
+        },
+      });
+    } catch (error) {
+      console.error('500 Internal Server Error:', error);
+    }
+  }
 
-  // @Post('post')
-  // async createDraft(
-  //   @Body() postData: { title: string; content?: string; authorEmail: string },
-  // ): Promise<TravelDestinationModel> {
-  //   const { title, content, authorEmail } = postData;
-  //   return this.travelService.createTravelDestination({
-  //     title,
-  //     content,
-  //     author: {
-  //       connect: { email: authorEmail },
-  //     },
-  //   });
-  // }
+  @Put('travels/:id')
+  async updateTravel(
+    @Param('id') id: string,
+    @Body()
+    putData: {
+      name: string;
+      location?: string;
+      estimated_budget?: string;
+      description?: string;
+      image: string;
+      authorId: number;
+    },
+  ): Promise<TravelDestinationModel> {
+    console.log('Received PUT Data:', putData);
+    const { name, location, estimated_budget, description, image } = putData;
+    return this.travelService.updateTravelDestination({
+      where: { id: Number(id) },
+      data: {
+        name: name,
+        location: location,
+        estimated_budget: estimated_budget,
+        description: description,
+        image: image,
+      },
+    });
+  }
+
+  @Delete('travels/:id')
+  async deleteTravelDestination(
+    @Param('id') id: string,
+  ): Promise<TravelDestinationModel> {
+    return this.travelService.deleteTravelDestination({ id: Number(id) });
+  }
 
   @Post('user')
   async signupUser(
